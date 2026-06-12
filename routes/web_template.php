befd1d8b856db36a6891b0286769d6b164e06ff0
Imraan <sorghoimraan@gmail.com>
2026-06-12 12:18:32 +0000

    add temlates

--- routes/web_template.php
+++ routes/web_template.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,18 @@ Route::get('/', function () {
 Route::get('/components', function () {
     return view('components');
 });
+
+/*
+|------------------------------------------------------------
+| Maquettes HTML statiques (resources/views/templates)
+|------------------------------------------------------------
+*/
+Route::get('/templates/{file?}', function (string $file = 'index') {
+    $file = basename($file, '.html');
+    $path = resource_path("views/templates/{$file}.html");
+
+    abort_unless(File::exists($path), 404);
+
+    return response(File::get($path));
+})->where('file', '[A-Za-z0-9_-]+(\.html)?')->name('templates.show');
 
